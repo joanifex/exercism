7f1fb0f79036423e1b2fc0adbb6cc5dfa8ce1fef
Ian Wright <ian@authentise.com>
2018-03-21 17:11:20 -0600

    Run-length-encoding

---
+++ run-length-encoding/run-length-encoding.js
@@ -0,0 +1,9 @@
+let run = /(\w)\1+/g;
+let compressed = /(\d+)(\w)/g;
+
+module.exports = {
+  encode: string =>
+    string.replace(run, match => match.length + match.charAt(0)),
+  decode: encoded =>
+    encoded.replace(compressed, (match, length, char) => char.repeat(length)),
+};
